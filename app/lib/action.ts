4867686b8d6a7556ad17a028f9b10f402efe2167
sekine-1113 <sekine104@outlook.jp>
2025-05-25 16:12:28 +0900

    fix

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -101,7 +101,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
